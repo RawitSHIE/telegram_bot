357c90da54a82cab9318627202b2fbd7f92894ec
RawitSHIE <rawitgun@gmail.com>
2018-04-01 22:35:49 +0700

    git first

---
+++ index.js
@@ -0,0 +1,48 @@
+const TelegramBot = require('node-telegram-bot-api');
+const token = '572017543:AAFy1s2YoJf9a6kcUryIaoCKN9jUn9ftV_k';
+const bot = new TelegramBot(token, {polling: true});
+
+
+const usr = 454788039;
+setInterval(() => {
+    bot.sendMessage(usr, "hi");
+}, 5000);
+
+
+bot.on('message', (msg) => {
+    const check = "check";
+    if (msg.text.toString().toLowerCase().includes(check)) {
+        bot.sendMessage(msg.chat.id, "check");
+    } 
+    
+    const bye = "bye";
+    if (msg.text.toString().toLowerCase().includes(bye)) {
+        bot.sendMessage(msg.chat.id, "Hope to see you around again , Bye");
+    } 
+    
+    const robot = "robot";
+    if (msg.text.toString().toLowerCase().includes(robot)) {
+        bot.sendMessage(msg.chat.id, "http://www.google.com");
+    }
+
+    const swar = "line";
+    if (msg.text.includes(swar)) {
+        bot.kickChatMember(msg.chat.id,  msg.from.id);
+    }
+});
+
+bot.onText(/\/start/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Welcome");
+    });
+
+bot.onText(/\/keyboard/, (msg) => {
+    bot.sendMessage(msg.chat.id, "Select Option", {
+        "reply_markup": {
+            "keyboard": [["Alert", "Call 911"], ["Ignore"]]
+            }
+        });
+    });
+
+bot.onText(/\/sendpic/, (msg) => {
+    bot.sendPhoto(msg.chat.id,"https://www.somesite.com/image.jpg",{caption : "Here we go ! \nThis is just a caption "} );
+});
